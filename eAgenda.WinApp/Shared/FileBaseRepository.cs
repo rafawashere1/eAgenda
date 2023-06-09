40f89dab60eea06bfcf285271c9b3797e3a69a50
rafawashere1 <rafaelsantos138@hotmail.com>
2023-06-09 19:14:12 -0300

    Adicionado Repositorio de Arquivos Base

---
+++ eAgenda.WinApp/Shared/FileBaseRepository.cs
@@ -0,0 +1,123 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json;
+using System.Xml.Serialization;
+
+namespace eAgenda.WinApp.Shared
+{
+    public abstract class FileBaseRepository<T> where T : BaseEntity<T>
+    {
+        protected int counter;
+
+        protected List<T> registers;
+
+        public FileBaseRepository()
+        {
+            if (File.Exists(GetFileName()))
+                LoadRegistersFromJsonFile();
+
+            registers = new List<T>();
+        }
+
+        public void Insert(T register)
+        {
+            counter++;
+
+            register.Id = counter;
+
+            registers.Add(register);
+
+            SendRegistersToJsonFile();
+        }
+
+        public void Edit(int id, T register)
+        {
+            SelectById(id).UpdateInfos(register);
+
+            SendRegistersToJsonFile();
+        }
+
+        public void Delete(T register)
+        {
+            registers.Remove(register);
+
+            SendRegistersToJsonFile();
+        }
+
+        public T SelectById(int id)
+        {
+            return registers.FirstOrDefault(x => x.Id == id);
+        }
+
+        public List<T> GetAll()
+        {
+            return registers;
+        }
+
+        private void UpdateCounter()
+        {
+            counter = registers.Max(x => x.Id);
+        }
+
+        #region Binary Serializer
+        protected void SendRegistersToBinaryFile()
+        {
+            BinaryFormatter serializer = new();
+
+            MemoryStream stream = new();
+
+            serializer.Serialize(stream, registers);
+
+            File.WriteAllBytes(GetFileName(), stream.ToArray());
+        }
+
+        protected void LoadRegistersFromBinaryFile()
+        {
+            BinaryFormatter serializer = new();
+
+            MemoryStream stream = new(File.ReadAllBytes(GetFileName()));
+
+            registers = serializer.Deserialize(stream) as List<T>;
+
+            UpdateCounter();
+        }
+        #endregion
+
+        #region Json Serializer
+        protected void SendRegistersToJsonFile()
+        {
+            File.WriteAllText(GetFileName(), JsonSerializer.Serialize(registers));
+        }
+
+        protected void LoadRegistersFromJsonFile()
+        {
+            registers = JsonSerializer.Deserialize<List<T>>(File.ReadAllText(GetFileName()));
+        }
+        #endregion
+
+        #region Xml Serializer
+        protected void SendRegistersToXmlFile()
+        {
+            XmlSerializer serializer = new(typeof(List<T>));
+
+            MemoryStream stream = new();
+
+            serializer.Serialize(stream, registers);
+
+            File.WriteAllBytes(GetFileName(), stream.ToArray());
+        }
+
+        protected void LoadRegistersFromXmlFile()
+        {
+            XmlSerializer serializer = new(typeof(List<T>));
+
+            MemoryStream stream = new(File.ReadAllBytes(GetFileName()));
+
+            registers = serializer.Deserialize(stream) as List<T>;
+
+            UpdateCounter();
+        }
+        #endregion
+
+        protected abstract string GetFileName();
+    }
+}
--- eAgenda.WinApp/TaskModule/FileTaskRepository.cs
+++ eAgenda.WinApp/TaskModule/FileTaskRepository.cs
@@ -1,125 +1,31 @@
-﻿using System.Runtime.Serialization.Formatters.Binary;
-using System.Text.Json;
-using System.Xml.Serialization;
-
-namespace eAgenda.WinApp.TaskModule
+﻿namespace eAgenda.WinApp.TaskModule
 {
-    public class FileTaskRepository : ITaskRepository
+    public class FileTaskRepository : FileBaseRepository<Task>, ITaskRepository
     {
-        private int _counter;
-
-        private List<Task> _tasks = new();
-
-        private const string TASK_FILE_NAME = "TaskModule/Tasks.json";
 
         public FileTaskRepository()
         {
-            if (File.Exists(TASK_FILE_NAME))
-                LoadTasksFromJsonFile();
+            if (File.Exists(GetFileName()))
+                LoadRegistersFromJsonFile();
         }
-
-        public void Insert(Task task)
-        {
-            _counter++;
-
-            task.Id = _counter;
-
-            _tasks.Add(task);
-            
-            SendTasksToJsonFile();
-        }
-
-        public void Edit(int id, Task task)
-        {
-            SelectById(id).UpdateInfos(task);
-
-            SendTasksToJsonFile();
-        }
-
-        public void Delete(Task task)
-        {
-            _tasks.Remove(task);
-            
-            SendTasksToJsonFile();
-        }
-
-        public Task SelectById(int id)
-        {
-            return _tasks.FirstOrDefault(x => x.Id == id);
-        }
-
         public List<Task>? SelectMarkeds()
         {
-            return _tasks.Where(x => x.CompletionPercentage == 100).ToList();
+            return registers.Where(x => x.CompletionPercentage == 100).ToList();
         }
 
         public List<Task>? SelectUnmarkeds()
         {
-            return _tasks.Where(x => x.CompletionPercentage < 100).ToList();
+            return registers.Where(x => x.CompletionPercentage < 100).ToList();
         }
 
         public List<Task>? SelectAllOrdenedByPriority()
         {
-            return _tasks.OrderByDescending(x => x.Priority).ToList();
+            return registers.OrderByDescending(x => x.Priority).ToList();
         }
 
-        private void SendTasksToBinaryFile()
+        protected override string GetFileName()
         {
-            BinaryFormatter serializer = new();
-
-            MemoryStream taskStream = new();
-
-            serializer.Serialize(taskStream, _tasks);
-
-            File.WriteAllBytes(TASK_FILE_NAME, taskStream.ToArray());
-        }
-
-        private void LoadTasksFromBinaryFile()
-        {
-            BinaryFormatter serializer = new();
-
-            MemoryStream taskStream = new(File.ReadAllBytes(TASK_FILE_NAME));
-
-            _tasks = serializer.Deserialize(taskStream) as List<Task>;
-
-            UpdateCounter();
-        }
-
-        private void SendTasksToJsonFile()
-        {
-            File.WriteAllText(TASK_FILE_NAME, JsonSerializer.Serialize(_tasks));
-        }
-
-        private void LoadTasksFromJsonFile()
-        {
-            _tasks = JsonSerializer.Deserialize<List<Task>>(File.ReadAllText(TASK_FILE_NAME));
-        }
-
-        private void SendTasksToXmlFile()
-        {
-            XmlSerializer serializer = new(typeof(List<Task>));
-
-            MemoryStream taskStream = new();
-
-            serializer.Serialize(taskStream, _tasks);
-
-            File.WriteAllBytes(TASK_FILE_NAME, taskStream.ToArray());
-        }
-
-        private void LoadTasksFromXmlFile()
-        {
-            XmlSerializer serializer = new(typeof(List<Task>));
-
-            MemoryStream taskStream = new(File.ReadAllBytes(TASK_FILE_NAME));
-
-            _tasks = serializer.Deserialize(taskStream) as List<Task>;
-
-            UpdateCounter();
-        }
-
-        private void UpdateCounter()
-        {
-            _counter = _tasks.Max(x => x.Id);
+            return "TaskModule/Tasks.json";
         }
     }
 }
